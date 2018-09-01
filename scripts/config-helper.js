c0b676a85d98acafd3a6e9f8a23dbfa3631c6f9e
Longweibing <591178251@qq.com>
2018-09-01 10:49:45 +0800

    1
    1

--- scripts/config-helper.js
+++
@@ -1,8 +0,0 @@
-hexo.on('generateBefore', function () {
-  if (hexo.locals.get) {
-    var data = hexo.locals.get('data')
-    if (data && data.lemonlime ) {
-    	hexo.theme.config = data.lemonlime;
-    }
-  }
-})
